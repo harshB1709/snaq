e792d596e9e8c52af2aa0136672b4f2f10455ac0
Harsh Bachawat <harsh.bachawat@ranium.in>
2025-03-06 19:05:51 +0530

    custom email template

---
+++ resources/views/vendor/mail/html/header.blade.php
@@ -0,0 +1,10 @@
+@props(['url'])
+<tr>
+<td class="header">
+<a href="{{ $url }}" style="display: inline-block;">
+<div class="logo-container">
+	<img src="https://snake-game.ranium.in/images/game-logo.png" class="logo" alt="Laravel Logo">
+</div>
+</a>
+</td>
+</tr>
