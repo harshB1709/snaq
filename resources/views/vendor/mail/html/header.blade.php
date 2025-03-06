b5a1dbdb34d2e3e349351ea4322e5012e3d4044e
Harsh Bachawat <harsh.bachawat@ranium.in>
2025-03-06 19:26:48 +0530

    final mail changes

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,8 +1,8 @@
 @props(['url'])
 <tr>
 <td class="header">
-<a href="{{ $url }}" style="display: inline-block;">
-<div class="logo-container">
+<a href="{{ $url }}" style="display: inline-block;" class="logo-container">
+<div>
 	<img src="https://snake-game.ranium.in/images/game-logo.png" class="logo" alt="Laravel Logo">
 </div>
 </a>
